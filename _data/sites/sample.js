a2ecff9ccc6f0d35638938ef321b97da13c083a9
stardoom4 <139852159+stardoom4@users.noreply.github.com>
2024-02-08 01:46:14 +0530

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
+	name: "My Personal Site", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		runs: 1,
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://garud3.netlify.app"
 	]
 };
