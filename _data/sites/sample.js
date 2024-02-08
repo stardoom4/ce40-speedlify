10a0391cbb9a58cfa6b427756708536e87a9dd4a
stardoom4 <139852159+stardoom4@users.noreply.github.com>
2024-02-08 09:30:11 +0530

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,11 +1,14 @@
 module.exports = {
-	name: "My Personal Site", // optional, falls back to object key
-	description: "The default sites that get tested",
+	name: "Garud", // optional, falls back to object key
+	description: "My Personal Site",
 	options: {
 		runs: 1,
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://garud3.netlify.app"
+		"https://garud.netlify.app"
+		"https://garud.netlify.app/notes/"
+                "https://garud.netlify.app/microblog/'
+		"https://garud.netlify.app/extra/"
 	]
 };
