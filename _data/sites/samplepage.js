0de8e378f0a4fb0d8bbe8d36bbf069512eee93e7
stardoom4 <139852159+stardoom4@users.noreply.github.com>
2024-02-08 09:43:34 +0530

    Create samplepage.js

---
+++ _data/sites/samplepage.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Garud-samplepage", // optional, falls back to object key
+	description: "The experimental page of my site",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://garud.netlify.app/sample/"
+	]
+};
