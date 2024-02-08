863d1790a19867ed5546cdfbeaece0c170bc7160
stardoom4 <139852159+stardoom4@users.noreply.github.com>
2024-02-08 09:37:31 +0530

    Create note.js

---
+++ _data/sites/note.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Garud", // optional, falls back to object key
+	description: "Notes page",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://garud.netlify.app/notes/"
+	]
+};
