5c16f00d03fd6f997b013c46bf5c21bb48bf8899
stardoom4 <139852159+stardoom4@users.noreply.github.com>
2024-02-08 09:38:14 +0530

    Create microblog.js

---
+++ _data/sites/microblog.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Garud", // optional, falls back to object key
+	description: "MicroBlog page",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://garud.netlify.app/microblog/"
+	]
+};
